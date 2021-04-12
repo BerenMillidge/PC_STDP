15b43cfef6f08b64a7ec0555848efba962071b59
BerenMillidge <bmillidgework@gmail.com>
2021-04-12 16:23:50 +0100

    initial

---
+++ tests2.jl
@@ -0,0 +1,43 @@
+a = 5
+b = 6
+
+using Plots
+
+function single_exp_kernel(series, t_rise)
+    int_1 = 0
+    int_1s = zeros(length(series))
+    for (i,el) in enumerate(series)
+        int_1 += -(int_1 / t_rise) + el
+        int_1s[i] = int_1
+    end
+    return int_1s
+end
+function double_exp_kernel(series, t_rise, t_decay)
+    int_1 = 0
+    int_2 = 0
+    int_1s = []
+    int_2s = []
+    for el in series
+        int_1 += -(int_1 / t_rise) + el
+        int_2 += (int_1 - int_2) / t_decay
+        push!(int_1s, int_1)
+        push!(int_2s, int_2)
+    end
+    return int_2, int_1s, int_2s
+end
+
+t_rise = 2
+series = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+outs = single_exp_kernel(series, t_rise)
+plot(outs)
+
+series_pre = zeros(30)
+series_post = zeros(30)
+series_pre[10] = 1
+series_post[12] = 1
+pre = single_exp_kernel(series_pre, t_rise)
+post = single_exp_kernel(series_post, t_rise)
+plot(post .- pre)
+plot!(post)
+plot!(pre)
+plot((post .- pre) .* pre)
