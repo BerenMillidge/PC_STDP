df53592ec3630de6c8fd432f59cc03784772a400
BerenMillidge <bmillidgework@gmail.com>
2021-04-13 22:50:33 +0100

    simplest plot

---
+++ simple_stdp.jl
@@ -0,0 +1,20 @@
+# simple simulation of STDP in the simplest possible case just to see what the graph looks like
+
+using Plots
+
+function stdp_update(dt, coeff, decay)
+    if dt == 0
+        return 0
+    elseif dt > 0
+        return coeff * exp(-dt / decay)
+    elseif dt < 0
+        return -coeff * exp(dt / decay)
+    else
+        return -1e6
+    end
+end
+post_t = 25 # half way through
+xs = [i for i in -24:25]
+stdps = stdp_update.(xs, 1, 5)
+plot(xs, stdps)
+savefig("Prototypical_STDP")
